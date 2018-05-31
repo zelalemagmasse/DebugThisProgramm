cba8ace34adb70030cd4ca810c921498c814e593
zelalem <zelalem.agmasse@gmail.com>
2018-05-31 16:03:11 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Debuging a program
---
+++ src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        double x,y;
+        x=3.1415;
+        y=3.64;
+        System.out.println("pi is approximately" + x );
+        System.out.println("My GPA " + y);
+    }
+}
